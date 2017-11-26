d34ae1cff9802c512c384b14f4e35ece80f0d1da
Andreas Nordahl <andnorda@gmail.com>
2017-11-26 11:20:22 +0100

    First, simple graph

---
+++ main.js
@@ -0,0 +1,34 @@
+var request = new XMLHttpRequest()
+request.addEventListener('load', onLoad)
+request.open('GET', 'https://temp-5c997.firebaseio.com/temps.json')
+request.send()
+
+function onLoad() {
+  const data = Object.values(JSON.parse(this.responseText))
+    .map(m => ({
+      ...m,
+      time: new Date(m.timestamp * 1000),
+    }))
+    .filter(m => dateFns.getMinutes(m.time) === 0)
+
+  chart({
+    labels: data.map(m => dateFns.format(m.time, 'HH:mm')),
+    data: data.map(m => Number(m.temp)),
+  })
+}
+
+function chart({ labels, data }) {
+  new Chart(document.getElementById('myChart').getContext('2d'), {
+    type: 'line',
+    data: {
+      labels,
+      datasets: [
+        {
+          label: 'temp',
+          data,
+        },
+      ],
+    },
+    options: {},
+  })
+}
